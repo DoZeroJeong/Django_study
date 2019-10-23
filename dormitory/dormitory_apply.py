e1ae6500f910770ecca008ddbd14f05452d52db8
DoZeroJeong <dogug2865@gmail.com>
2019-10-24 00:22:02 +0900

    dormitory next month out apply add

--- dormitory/dormitory_apply.py
+++ dormitory/dormitory_apply.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.common.exceptions import UnexpectedAlertPresentException
 import time
+from datetime import datetime
 from selenium.webdriver.common.alert import Alert
 
+today = datetime.today().day
 
 def dormitory(uid, upasswd, first, second, apply_text):
-    # display = Display(visible=0, size=(800, 800))
-    # display.start()
     driver = webdriver.Chrome('C:/Users/Administrator/Desktop/chromedriver_win32/chromedriver')  # 웹브라우저 chrome
     driver.get("http://dormitory.tu.ac.kr/default/main/main.jsp")
     login_bt = driver.find_element_by_xpath('/html/body/div[2]/div[1]/ul[2]/li[2]/a/img')
@@ -34,22 +34,29 @@ def dormitory(uid, upasswd, first, second, apply_text):
         print("조회버튼 클릭")
         lookup_bt = driver.find_element_by_xpath('//*[@id="btnRetrieveStd"]')
         lookup_bt.click()
-        time.sleep(1)
         # 외박신청 일자 선택
         print("외박신청 일자 선택")
         apply = driver.find_element_by_name('txtSTAYOUT_REQ_FR_DT')
         apply.click()
-        time.sleep(1)
-        if first < second:
+        if first < str(today):
+            next_month = driver.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/a[2]')
+            next_month.click()
+            first_day = driver.find_element_by_xpath('//*[text() = ' + first + ']')  # text 추출
+            first_day.click()
+            apply = driver.find_element_by_xpath('//*[@id="txtSTAYOUT_REQ_TO_DT"]')
+            apply.click()
+            next_month = driver.find_element_by_xpath('//*[@id="ui-datepicker-div"]/div/a[2]/span')
+            next_month.click()
+            second_day = driver.find_element_by_xpath('//*[text() = ' + second + ']')  # text 추출
+            second_day.click()
+        elif first < second:
             print("첫번째 외박일 선택")
             first_day = driver.find_element_by_xpath('//*[text() = ' + first + ']')  # text 추출
-            time.sleep(1)
             first_day.click()
             apply = driver.find_element_by_xpath('//*[@id="txtSTAYOUT_REQ_TO_DT"]')
             apply.click()
             print("두번째 외박인 선택")
             second_day = driver.find_element_by_xpath('//*[text() = ' + second + ']')  # text 추출
-            time.sleep(1)
             second_day.click()
         else:
             first_day = driver.find_element_by_xpath('//*[text() = ' + first + ']')  # text 추출
@@ -63,7 +70,6 @@ def dormitory(uid, upasswd, first, second, apply_text):
             second_day.click()
         # 외박사유 입력
         text = driver.find_element_by_name('txtBIGO')
-        time.sleep(1)
         text.send_keys(apply_text)
         print("dormitory out reason")
         # 외박신청 버튼 클릭
@@ -81,5 +87,3 @@ def dormitory(uid, upasswd, first, second, apply_text):
     finally:
         driver.quit()
         print("드라이브 종료")
-        # display.stop()
-        # print("디스플레이 종료")
